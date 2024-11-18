98ff63c42862b95f6a62b9ebf6fce846a9f6090b
roel <ash49901@gmail.com>
2024-11-18 13:08:50 +0900

    Feat : SplashScreen 화면 구현

---
+++ app/src/main/java/com/daon/onjung/ui/splash/SplashScreen.kt
@@ -0,0 +1,54 @@
+package com.daon.onjung.ui.splash
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.daon.onjung.R
+import com.daon.onjung.ui.theme.OnjungTheme
+
+@Composable
+fun SplashScreen(
+
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Icon(
+                painter = painterResource(id = R.drawable.ic_app_logo),
+                contentDescription = "App Logo",
+                tint = OnjungTheme.colors.main_coral
+            )
+            Spacer(modifier = Modifier.height(44.dp))
+            Text(
+                text = "온정과 시작하는\n 선행의 선순환",
+                style = OnjungTheme.typography.h2.copy(
+                    color = OnjungTheme.colors.main_coral,
+                    lineHeight = 30.sp
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    OnjungTheme {
+        SplashScreen()
+    }
+}
