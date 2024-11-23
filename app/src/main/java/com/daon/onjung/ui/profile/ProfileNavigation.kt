39bcfe3425d8dcc99acb09504f3ee82e2b9e7304
An Sehong <ash49901@gmail.com>
2024-11-24 04:25:38 +0900

    BugFix : 식당 갯수 오류 수정 (#21)

--- app/src/main/java/com/daon/onjung/ui/profile/ProfileNavigation.kt
+++ app/src/main/java/com/daon/onjung/ui/profile/ProfileNavigation.kt
@@ -2,7 +2,9 @@ package com.daon.onjung.ui.profile
 
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.daon.onjung.OnjungAppState
 import com.daon.onjung.OnjungBottomSheetState
 import com.daon.onjung.Routes
@@ -34,14 +36,22 @@ fun NavGraphBuilder.profileGraph(
     }
 
     composable(
-        route = Routes.Profile.TICKET_LIST
-    ) {
+        route = "${Routes.Profile.TICKET_LIST}?ticketCount={ticketCount}",
+        arguments = listOf(
+            navArgument("shopId") {
+                type = NavType.StringType
+                defaultValue = ""
+            }
+        )
+    ) { entry ->
         val viewModel: ProfileTicketListViewModel = hiltViewModel()
-
+        val ticketCountString = entry.arguments?.getString("ticketCount") ?: "0"
+        val ticketCount = ticketCountString.toInt()
         ProfileTicketListScreen(
             appState = appState,
             bottomSheetState = bottomSheetState,
-            viewModel = viewModel
+            viewModel = viewModel,
+            ticketCount = ticketCount
         )
     }
 }
