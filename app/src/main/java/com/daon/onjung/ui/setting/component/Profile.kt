668dd089b9cb38c9f0efa59b114dcaf711cd7879
dongchyeon <kimdonghyun90766@gmail.com>
2024-11-27 22:44:04 +0900

    Refactor : ProfileImage 컴포넌트 제거

--- app/src/main/java/com/daon/onjung/ui/setting/component/Profile.kt
+++ app/src/main/java/com/daon/onjung/ui/setting/component/Profile.kt
@@ -5,22 +5,35 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.error
+import coil3.request.fallback
+import com.daon.onjung.R
 import com.daon.onjung.ui.theme.OnjungTheme
 
 @Preview(showBackground = true)
 @Composable
 fun Profile(
+    modifier: Modifier = Modifier,
     name: String = "온정이",
     imageUrl: String = "",
-    modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -28,8 +41,18 @@ fun Profile(
         verticalAlignment = Alignment.CenterVertically,
 
     ) {
-        ProfileImage(
-            imageUrl = imageUrl
+        AsyncImage(
+            modifier = modifier
+                .size(60.dp)
+                .clip(CircleShape),
+            model = ImageRequest.Builder(context)
+                .data(imageUrl)
+                .error(R.drawable.ic_profile_icon)
+                .fallback(R.drawable.ic_profile_icon)
+                .build(),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Profile Image",
+            placeholder = painterResource(id = R.drawable.ic_profile_icon)
         )
         Spacer(modifier = Modifier.width(18.dp))
         ProfileInfo(
