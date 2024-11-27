5860c3a5c54a7b00b6ea915f92b5d98c56817cb4
dongchyeon <kimdonghyun90766@gmail.com>
2024-11-27 12:48:36 +0900

    #50 Feat : 게시글 등록 API Data Layer 세팅

---
+++ app/src/main/java/com/daon/onjung/data/datasource/SuggestionDataSource.kt
@@ -0,0 +1,16 @@
+package com.daon.onjung.data.datasource
+
+import com.daon.onjung.network.adapter.ApiResult
+import com.daon.onjung.network.model.BaseResponse
+import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+
+interface SuggestionDataSource {
+
+    suspend fun postBoard(
+        body: RequestBody,
+        file: MultipartBody.Part?
+    ) : Flow<ApiResult<BaseResponse<Unit>>>
+
+}
